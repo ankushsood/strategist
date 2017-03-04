85ac2e8de7d38e19f551a0971501b30f784933f5
Ankush Sood <sood.ankush@gmail.com>
2017-03-04 09:02:38 +0530

    Updated Login Page, LEft MEnu and top header with new theme

--- src/main/webapp/src/admin/admin.routes.js
+++ src/main/webapp/src/admin/admin.routes.js
@@ -12,14 +12,53 @@
 	 // $httpProvider.interceptors.push('loginHttpInterceptor');
 
 	  // Routes
-	  $stateProvider
+	 /* $stateProvider
 		.state('home', {
 		  url: '/admin/home',
-		  templateUrl: 'src/admin/home.html',
+		  templateUrl: 'src/admin/menu/leftMenu.html',
 		  controller : 'AdminHomeController',
 		  controllerAs: 'adminHomeCtrl',
 
 	  });
+	  
+	  */
+	  
+		$stateProvider
+		.state('home', {
+			url: '/admin/home',
+			views: {
+			  '@' : {
+				templateUrl: 'layout.html'
+			  },
+			  'top@home' : 	{
+								templateUrl: 'src/admin/menu/adminTop.html'
+							},
+			  'left@home' : {	
+								templateUrl: 'src/admin/menu/leftMenu.html',
+								controller : 'AdminHomeController'
+							},
+			  'main@home' : { 
+								templateUrl: 'src/admin/menu/adminMain.html'
+							}
+			},
+		  })
+		.state('home.menu', {
+			url: '/adminMenu',
+		  templateUrl: 'src/admin/menu/leftMenu.html',
+		  controller : 'AdminHomeController',
+		  controllerAs: 'adminHomeCtrl',
+		  })
+		.state('home.menu.detail', {
+			url: '/adminHome',
+			views: {
+			  'detail@home' : {
+				  templateUrl: 'src/admin/menu/leftMenu.html',
+				  controller : 'AdminHomeController',
+				  controllerAs: 'adminHomeCtrl'
+			  },
+			},
+		  })
+  
 	}
 
 })();
